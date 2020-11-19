0a82463dc35067e88afdc36597889567cdf0fd1a
Student User <student@workstation.lab.example.com>
2020-11-19 13:26:35 -0500

    Fixed linting errors

--- books/routes/authors.js
+++ books/routes/authors.js
@@ -3,9 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 const authors = require('../data/authors');
-
-var user;
-
 /* GET users listing. */
 router.get('/', (req, res) => {
   res.json(authors);
